9358da047371a46026f22bad276b9d9c59f24f0d
Roman Derevianko <r2307v@gmail.com>
2020-01-21 00:01:43 +0200

    Add TextField, TextView

---
+++ Extend/Views/TextView/TextView.swift
@@ -0,0 +1,82 @@
+//
+//  TextView.swift
+//  Extend
+//
+//  Created by Roman Derevianko on 20.01.2020.
+//  Copyright © 2020 Roman Derevianko. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+open class TextView: UITextView {
+  // MARK: - Properties
+  
+  open var placeholder: String? {
+    didSet { setNeedsDisplay() }
+  }
+  
+  open var placeholderColor: UIColor = .lightGray {
+    didSet { setNeedsDisplay() }
+  }
+  
+  open var placeholderAttributes: [NSAttributedString.Key : Any]? {
+    didSet { setNeedsDisplay() }
+  }
+  
+  open var attributedPlaceholder: NSAttributedString {
+    return NSAttributedString(string: placeholder ?? "", attributes: placeholderAttributes ?? [
+      NSAttributedString.Key.foregroundColor : placeholderColor,
+      NSAttributedString.Key.font : font ?? UIFont.systemFont(ofSize: UIFont.systemFontSize),
+      NSAttributedString.Key.paragraphStyle : NSMutableParagraphStyle().then {
+        $0.alignment = textAlignment
+      }
+    ])
+  }
+  
+  open override var text: String! {
+    didSet { setNeedsDisplay() }
+  }
+  
+  // MARK: - Inits
+  
+  public override init(frame: CGRect, textContainer: NSTextContainer?) {
+    super.init(frame: frame, textContainer: textContainer)
+    commonInit()
+  }
+  
+  public required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    commonInit()
+  }
+  
+  private func commonInit() {
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(textDidChange(_:)),
+                                           name: UITextView.textDidChangeNotification,
+                                           object: nil)
+  }
+  
+  deinit {
+    NotificationCenter.default.removeObserver(self)
+  }
+  
+  // MARK: - Drawing
+  
+  open override func draw(_ rect: CGRect) {
+    super.draw(rect)
+    
+    if text.isEmpty {
+      let textRect = rect.inset(by: textContainerInset)
+        .insetBy(dx: textContainer.lineFragmentPadding, dy: 0)
+      attributedPlaceholder.draw(in: textRect)
+    }
+  }
+  
+  // MARK: - Notification handlers
+  
+  @objc open func textDidChange(_ notification: Notification) {
+    setNeedsDisplay()
+  }
+}
+
